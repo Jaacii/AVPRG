084f4dd63e96283f54a3608c3e64d839f66b4eee
Jaacii <sliphno@hotmail.de>
2018-11-09 16:21:46 +0100

    test

--- roomModel.js
+++ roomModel.js
@@ -8,38 +8,6 @@ var isPlaying = false;
 var playStopButton = document.getElementById("playStopButton");
 sound.loop = true;
 
-let dimensions = {
-  small: {width: 1.5, height: 2.4, depth: 1.3},
-  medium: {width: 4, height: 3.2, depth: 3.9},
-  large: {width: 8, height: 3.4, depth: 9},
-  huge: {width: 20, height: 10, depth: 20},
-};
-
-let materials = {
-  brick: {
-    left: "brick-bare", right: "brick-bare",
-    up: "brick-bare", down: "wood-panel",
-    front: "brick-bare", back: "brick-bare",
-  },
-  curtains: {
-    left: "curtain-heavy", right: "curtain-heavy",
-    up: "wood-panel", down: "wood-panel",
-    front: "curtain-heavy", back: "curtain-heavy",
-  },
-  marble: {
-    left: "marble", right: "marble",
-    up: "marble", down: "marble",
-    front: "marble", back: "marble",
-  },
-  outside: {
-    left: "transparent", right: "transparent",
-    up: "transparent", down: "grass",
-    front: "transparent", back: "transparent",
-  },
-};
-
-let dimensionSelection = "small";
-let materialSelection = "brick";
 let canvasControl;
 let Bonus2Scene;
 let soundSource;
